97ce5dc5e0498e29addd56d5169e617ee4e0eb4e
Boyang Li <boyang.li@cern.ch>
2019-01-07 11:22:16 -0800

    move write into Loop func

--- include/LinkDef.h
+++ include/LinkDef.h
@@ -1,8 +1,8 @@
 #ifdef __MAKECINT__
-#pragma link C++ class std::vector < std::vector<int> >*+;
-#pragma link C++ class std::vector < std::vector<char> >*+;
-#pragma link C++ class std::vector < std::vector<float> >*+;
-#pragma link C++ class std::vector < std::vector<double> >*+;
-#pragma link C++ class std::vector < std::vector<short> >*+;
-#pragma link C++ class std::vector < std::vector<unsigned char> >*+;
+#pragma link C++ class < std::vector<int> >+;
+#pragma link C++ class < std::vector<char> >+;
+#pragma link C++ class < std::vector<float> >+;
+#pragma link C++ class < std::vector<double> >+;
+#pragma link C++ class < std::vector<short> >+;
+#pragma link C++ class < std::vector<unsigned char> >+;
 #endif
