0e00dd58ae6b1ee6a1902a2519595cc2b5f24d75
Boyang Li <boyang.li@cern.ch>
2019-01-18 16:00:21 -0800

    tmva to root6

--- util/trainBDT.cc
+++ util/trainBDT.cc
@@ -1,35 +1,9 @@
-// @(#)root/tmva $Id$
-/**********************************************************************************
- * Project   : TMVA - a Root-integrated toolkit for multivariate data analysis    *
- * Package   : TMVA                                                               *
- * Root Macro: TMVARegression                                                     *
- *                                                                                *
- * This macro provides examples for the training and testing of the               *
- * TMVA classifiers.                                                              *
- *                                                                                *
- * As input data is used a toy-MC sample consisting of four Gaussian-distributed  *
- * and linearly correlated input variables.                                       *
- *                                                                                *
- * The methods to be used can be switched on and off by means of booleans, or     *
- * via the prompt command, for example:                                           *
- *                                                                                *
- *    root -l TMVARegression.C\(\"LD,MLP\"\)                                      *
- *                                                                                *
- * (note that the backslashes are mandatory)                                      *
- * If no method given, a default set is used.                                     *
- *                                                                                *
- * The output file "TMVAReg.root" can be analysed with the use of dedicated       *
- * macros (simply say: root -l <macro.C>), which can be conveniently              *
- * invoked through a GUI that will appear at the end of the run of this macro.    *
- **********************************************************************************/
-
 #include <cstdlib>
 #include <iostream> 
 #include <map>
 #include <string>
 #include "stdio.h"
 #include "stdlib.h"
-
 #include "TChain.h"
 #include "TFile.h"
 #include "TTree.h"
@@ -38,250 +12,69 @@
 #include "TSystem.h"
 #include "TROOT.h"
 #include "TMVA/TMVAGui.h"
-
-
 #if not defined(__CINT__) || defined(__MAKECINT__)
 #include "TMVA/Tools.h"
 #include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
 #endif
-
 using namespace TMVA;
-
 void RunMVA( TString region = "", TCut cut = "(EventNumber%2)!=0" , TString weightfile = "", TString ncuts = "", TString ntrees = "") 
 {
   char str[300];
-   // This loads the library
    TMVA::Tools::Instance();
-/*
-   // to get access to the GUI and all tmva macros
-   TString tmva_dir(TString(gRootDir) + "/tmva");
-   if(gSystem->Getenv("TMVASYS"))
-      tmva_dir = TString(gSystem->Getenv("TMVASYS"));
-   gROOT->SetMacroPath(tmva_dir + "/test/:" + gROOT->GetMacroPath() );
-   gROOT->ProcessLine(".L TMVARegGui.C");
-*/
-   // Default MVA methods to be trained + tested
-   std::map<std::string,int> Use;
-
-   // --- Mutidimensional likelihood and Nearest-Neighbour methods
-   Use["PDERS"]           = 0;
-   Use["PDEFoam"]         = 0; 
-   Use["KNN"]             = 1;
-   // 
-   // --- Linear Discriminant Analysis
-   Use["LD"]		  = 1;
-   // 
-   // --- Function Discriminant analysis
-   Use["FDA_GA"]          = 1;
-   Use["FDA_MC"]          = 0;
-   Use["FDA_MT"]          = 0;
-   Use["FDA_GAMT"]        = 0;
-   // 
-   // --- Neural Network
-   Use["MLP"]             = 1; 
-   // 
-   // --- Support Vector Machine 
-   Use["SVM"]             = 0;
-   // 
-   // --- Boosted Decision Trees
-   Use["BDT"]             = 0;
-   Use["BDTG"]            = 1;
-   // ---------------------------------------------------------------
-
    std::cout << std::endl;
    std::cout << "==> Start TMVARegression" << std::endl;
-
    TString myMethodList = "BDTG";
-   // Select methods (don't look at this code - not of interest)
-   if (myMethodList != "") {
-      for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) it->second = 0;
-
-      std::vector<TString> mlist = gTools().SplitString( myMethodList, ',' );
-      for (UInt_t i=0; i<mlist.size(); i++) {
-         std::string regMethod(mlist[i]);
-
-         if (Use.find(regMethod) == Use.end()) {
-            std::cout << "Method \"" << regMethod << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
-            for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) std::cout << it->first << " ";
-            std::cout << std::endl;
-            return;
-         }
-         Use[regMethod] = 1;
-      }
-   }
-
-   // --------------------------------------------------------------------------------------------------
-
-   // --- Here the preparation phase begins
-   // Create a new root output file
-   //TString outfileName( "output.root" );
    TFile* outputFile = TFile::Open( region+"_out.root", "RECREATE" );
-
-   // Create the factory object. Later you can choose the methods
-   // whose performance you'd like to investigate. The factory is 
-   // the only TMVA object you have to interact with
-   //
-   // The first argument is the base of the name of all the
-   // weightfiles in the directory weight/
-   //
-   // The second argument is the output file for the training results
-   // All TMVA output can be suppressed by removing the "!" (not) in
-   // front of the "Silent" argument in the option string
    TMVA::Factory *factory = new TMVA::Factory( region + weightfile, outputFile,
                                                "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
-
-   // Define the input variables that shall be used for the MVA training
-   // note that you may also use variable expressions, such as: "3*var1/var2*abs(var3)"
-   // [all types of expressions that can also be parsed by TTree::Draw( "expression" )]
+   TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
 
    if(region != "reg1l1tau1b2j") {
-      factory->AddVariable("t1mass",'F');
-      factory->AddVariable("wmass",'F');
-      factory->AddVariable("t1vismass",'F');
-      factory->AddVariable("tau_subpt",'F');
+      dataloader->AddVariable("t1mass",'F');
+      dataloader->AddVariable("wmass",'F');
+      dataloader->AddVariable("t1vismass",'F');
+      dataloader->AddVariable("tau_subpt",'F');
    }
-   factory->AddVariable("tautaumass",'F');
-   factory->AddVariable("ttvismass",'F');
-   factory->AddVariable("t2mass",'F');
-   factory->AddVariable("x1fit", 'F');
-   factory->AddVariable("x2fit", 'F');
-   factory->AddVariable("t2vismass",'F');
-   factory->AddVariable("tau_leadpt",'F');
-   
-   // You can add so-called "Spectator variables", which are not used in the MVA training, 
-   // but will appear in the final "TestTree" produced by TMVA. This TestTree will contain the 
-   // input variables, the response values of all trained MVAs, and the spectator variables
-
-   // TFile *input = TFile::Open( "MVAInput.root" );
-
-   // std::cout << "--- TMVAClassification       : Using input file: " << input->GetName() << std::endl;
-   
-   // // --- Register the training and test trees
-
-   // TTree *signal     = (TTree*)input->Get("tree_sig");
-   // TTree *background = (TTree*)input->Get("tree_bkg");
+   dataloader->AddVariable("tautaumass",'F');
+   dataloader->AddVariable("ttvismass",'F');
+   dataloader->AddVariable("t2mass",'F');
+   dataloader->AddVariable("x1fit", 'F');
+   dataloader->AddVariable("x2fit", 'F');
+   dataloader->AddVariable("t2vismass",'F');
+   dataloader->AddVariable("tau_leadpt",'F');
    TChain* signal = new TChain(region);
    TChain* background = new TChain(region);
-
    TString inputsignames[] = {"fcnc_ch","fcnc_uh"};
-
    TString inputbkgnames[] = {"Vjets","diboson","ttV","ttbarnohad","other","ttH","ttbargamma"};
-
    TString prefix = PACKAGE_DIR;
    prefix += "/data/reduce2/";
    for (int i = 0; i < 2; ++i)
       signal->Add(prefix + inputsignames[i]+"_tree.root");
    for (int i = 0; i < 7; ++i)
       background->Add(prefix + inputbkgnames[i]+"_tree.root");
-
    TCut mycuts = "";
    TCut mycutb = "";
-
    printf(" >>>> Training signal events: %d\n",(Int_t)signal->GetEntries((const char*)(mycuts && cut)));
    printf(" >>>> Training background events: %d\n",(Int_t)background->GetEntries((const char*)(mycutb && cut)));
-
-   factory->AddTree(signal, "Signal", 1.0, mycuts && cut, Types::kTraining);
-   factory->AddTree(background, "Background", 1.0, mycutb && cut, Types::kTraining);
-
-   factory->AddTree(signal, "Signal", 1.0, mycuts && !cut, Types::kTesting);
-   factory->AddTree(background, "Background", 1.0, mycutb && !cut, Types::kTesting);
-
-   factory->SetSignalWeightExpression    ("weight*fakeSF");
-   factory->SetBackgroundWeightExpression("weight*fakeSF");
-
-   // factory->PrepareTrainingAndTestTree( mycuts, mycutb,
-   //                                      "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
-
-   // ---- Book MVA methods
-   //
-   // please lookup the various method configuration options in the corresponding cxx files, eg:
-   // src/MethoCuts.cxx, etc, or here: http://tmva.sourceforge.net/optionRef.html
-   // it is possible to preset ranges in the option string in which the cut optimisation should be done:
-   // "...:CutRangeMin[2]=-1:CutRangeMax[2]=1"...", where [2] is the third input variable
-
-   // PDE - RS method
-   if (Use["PDERS"])
-      factory->BookMethod( TMVA::Types::kPDERS, "PDERS", 
-                           "!H:!V:NormTree=T:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=40:NEventsMax=60:VarTransform=None" );
-   // And the options strings for the MinMax and RMS methods, respectively:
-   //      "!H:!V:VolumeRangeMode=MinMax:DeltaFrac=0.2:KernelEstimator=Gauss:GaussSigma=0.3" );   
-   //      "!H:!V:VolumeRangeMode=RMS:DeltaFrac=3:KernelEstimator=Gauss:GaussSigma=0.3" );   
-
-   if (Use["PDEFoam"])
-       factory->BookMethod( TMVA::Types::kPDEFoam, "PDEFoam", 
-			    "!H:!V:MultiTargetRegression=F:TargetSelection=Mpv:TailCut=0.001:VolFrac=0.0666:nActiveCells=500:nSampl=2000:nBin=5:Compress=T:Kernel=None:Nmin=10:VarTransform=None" );
-
-   // K-Nearest Neighbour classifier (KNN)
-   if (Use["KNN"])
-      factory->BookMethod( TMVA::Types::kKNN, "KNN", 
-                           "nkNN=20:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim" );
-
-   // Linear discriminant
-   if (Use["LD"])
-      factory->BookMethod( TMVA::Types::kLD, "LD", 
-                           "!H:!V:VarTransform=None" );
-
-	// Function discrimination analysis (FDA) -- test of various fitters - the recommended one is Minuit (or GA or SA)
-   if (Use["FDA_MC"]) 
-      factory->BookMethod( TMVA::Types::kFDA, "FDA_MC",
-                          "!H:!V:Formula=(0)+(1)*x0+(2)*x1:ParRanges=(-100,100);(-100,100);(-100,100):FitMethod=MC:SampleSize=100000:Sigma=0.1:VarTransform=D" );
-   
-   if (Use["FDA_GA"]) // can also use Simulated Annealing (SA) algorithm (see Cuts_SA options) .. the formula of this example is good for parabolas
-      factory->BookMethod( TMVA::Types::kFDA, "FDA_GA",
-                           "!H:!V:Formula=(0)+(1)*x0+(2)*x1:ParRanges=(-100,100);(-100,100);(-100,100):FitMethod=GA:PopSize=100:Cycles=3:Steps=30:Trim=True:SaveBestGen=1:VarTransform=Norm" );
-
-   if (Use["FDA_MT"]) 
-      factory->BookMethod( TMVA::Types::kFDA, "FDA_MT",
-                           "!H:!V:Formula=(0)+(1)*x0+(2)*x1:ParRanges=(-100,100);(-100,100);(-100,100);(-10,10):FitMethod=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=2:UseImprove:UseMinos:SetBatch" );
-
-   if (Use["FDA_GAMT"]) 
-      factory->BookMethod( TMVA::Types::kFDA, "FDA_GAMT",
-                           "!H:!V:Formula=(0)+(1)*x0+(2)*x1:ParRanges=(-100,100);(-100,100);(-100,100):FitMethod=GA:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:Cycles=1:PopSize=5:Steps=5:Trim" );
-
-   // Neural network (MLP)
-   if (Use["MLP"])
-      factory->BookMethod( TMVA::Types::kMLP, "MLP", "!H:!V:VarTransform=Norm:NeuronType=tanh:NCycles=20000:HiddenLayers=N+20:TestRate=6:TrainingMethod=BFGS:Sampling=0.3:SamplingEpoch=0.8:ConvergenceImprove=1e-6:ConvergenceTests=15:!UseRegulator" );
-
-   // Support Vector Machine
-   if (Use["SVM"])
-      factory->BookMethod( TMVA::Types::kSVM, "SVM", "Gamma=0.25:Tol=0.001:VarTransform=Norm" );
-
-   // Boosted Decision Trees
-   if (Use["BDT"])
-     factory->BookMethod( TMVA::Types::kBDT, "BDT",
-                           "!H:!V:NTrees=100:MinNodeSize=1.0%:BoostType=AdaBoostR2:SeparationType=RegressionVariance:nCuts=10:PruneMethod=CostComplexity:PruneStrength=30" );
-
+   dataloader->AddTree(signal, "Signal", 1.0, mycuts && cut, Types::kTraining);
+   dataloader->AddTree(background, "Background", 1.0, mycutb && cut, Types::kTraining);
+   dataloader->AddTree(signal, "Signal", 1.0, mycuts && !cut, Types::kTesting);
+   dataloader->AddTree(background, "Background", 1.0, mycutb && !cut, Types::kTesting);
+   dataloader->SetSignalWeightExpression    ("weight*fakeSF");
+   dataloader->SetBackgroundWeightExpression("weight*fakeSF");
    TString tmp_bdt_setting = "!H:!V:NTrees=";
-   if (Use["BDTG"])
-     factory->BookMethod( TMVA::Types::kBDT, "BDTG",
-                           tmp_bdt_setting + ntrees + "::BoostType=Grad:Shrinkage=0.1:UseBaggedBoost=True:BaggedSampleFraction=0.6:nCuts="+ncuts+":MaxDepth=2");//:NegWeightTreatment=PairNegWeightsGlobal" );
-   // --------------------------------------------------------------------------------------------------
-
-   // ---- Now you can tell the factory to train, test, and evaluate the MVAs
-
-   // Train MVAs using the set of training events
+   factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG",
+                           tmp_bdt_setting + ntrees + "::BoostType=Grad:Shrinkage=0.1:UseBaggedBoost=True:BaggedSampleFraction=0.6:nCuts="+ncuts+":MaxDepth=2");
    factory->TrainAllMethods();
-
-   // ---- Evaluate all MVAs using the set of test events
    factory->TestAllMethods();
-
-   // ----- Evaluate and compare performance of all configured MVAs
    factory->EvaluateAllMethods();    
-
-   // --------------------------------------------------------------
-   
-   // Save the output
    outputFile->Close();
-
    std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
    std::cout << "==> TMVAClassification is done!" << std::endl;      
-
    delete factory;
-
-   // Launch the GUI for the root macros
    if (!gROOT->IsBatch()) TMVAGui( outputFile->GetName() );
 }
-
 int main(int argc, char const *argv[])
 {
    if (argc!=5)
@@ -302,4 +95,3 @@ int main(int argc, char const *argv[])
    }
    return 0;
 }
-
