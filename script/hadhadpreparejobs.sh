766d0a5668daace2823d6d56d9129d4050f58edd
Boyang Li <boyang.li@cern.ch>
2019-03-14 18:10:20 -0700

    link

--- script/hadhadpreparejobs.sh
+++ script/hadhadpreparejobs.sh
@@ -4,18 +4,20 @@ if test $# -lt 1; then
   echo "Usage: ./hadhadpreparejobs.sh /datasets/path sample.txt"
 
 else
-
-for lines in $2
+mkdir -p ../../datafiles/xTFW/run
+for samplefiles in `find ../../datafiles/xTFW/*.txt`
+do
+for lines in `cat samplefiles`
 	for files in `find $1/*$lines*r9364*/*`
 	do
-		echo -n $lines" "$files >> ../../datafiles/xTFW/mc16a_$2
+		echo -n $lines" "$files >> ../../datafiles/xTFW/run/mc16a_$2
 	done
 	for files in `find $1/*$lines*r10201*/*`
 	do
-		echo -n $lines" "$files >> ../../datafiles/xTFW/mc16d_$2
+		echo -n $lines" "$files >> ../../datafiles/xTFW/run/mc16d_$2
 	done
 	for files in `find $1/*$lines*r10724*/*`
 	do
-		echo -n $lines" "$files >> ../../datafiles/xTFW/mc16e_$2
+		echo -n $lines" "$files >> ../../datafiles/xTFW/run/mc16e_$2
 	done
 done
