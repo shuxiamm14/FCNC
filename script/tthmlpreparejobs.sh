eb33ffd4a41780a50e9e30f11974fc95b238d397
Boyang Li <boyang.li@cern.ch>
2019-08-22 16:50:18 -0700

    fix path

--- script/tthmlpreparejobs.sh
+++ script/tthmlpreparejobs.sh
@@ -4,42 +4,42 @@ if test $# -lt 1; then
   echo "Usage: ./hadhadpreparejobs.sh /absolute/path/of/datasets"
 
 else
-mkdir -p $ttH_fakes_DIR/datafiles/xTFW/v2/run
-rm $ttH_fakes_DIR/datafiles/xTFW/v2/run/mc16*
-for samplefiles in `find "$ttH_fakes_DIR/datafiles/xTFW/v2/" -name *.txt`
+mkdir -p $ttH_fakes_DIR/datafiles/tthML/v2/run
+rm $ttH_fakes_DIR/datafiles/tthML/v2/run/mc16*
+for samplefiles in `find "$ttH_fakes_DIR/datafiles/tthML/v2/" -name *.txt`
 do
 	for lines in `cat $samplefiles`
 	do
-		for files in `find $1/mc/*$lines*r9364*/*`
+		for files in `find $1/*$lines*mc16a*.root`
 		do
-			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/mc16a_`echo $samplefiles | awk -F "/" '{print $NF}'` 
+			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/mc16a_`echo $samplefiles | awk -F "/" '{print $NF}'` 
 		done
-		for files in `find $1/mc/*$lines*r10201*/*`
+		for files in `find $1/*$lines*mc16d*.root`
 		do
-			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/mc16d_`echo $samplefiles | awk -F "/" '{print $NF}'`
+			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/mc16d_`echo $samplefiles | awk -F "/" '{print $NF}'`
 		done
-		for files in `find $1/mc/*$lines*r10724*/*`
+		for files in `find $1/*$lines*mc16e*.root`
 		do
-			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/mc16e_`echo $samplefiles | awk -F "/" '{print $NF}'`
+			echo $lines" "$files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/mc16e_`echo $samplefiles | awk -F "/" '{print $NF}'`
 		done
 	done
 done
-for files in `find $1/data/*data15*/*`
+for files in `find $1/*data15*.root`
 do
-	echo $files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/data1516.txt
+	echo $files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/data1516.txt
 done
 
-for files in `find $1/data/*data16*/*`
+for files in `find $1/*data16*.root`
 do
-	echo $files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/data1516.txt
+	echo $files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/data1516.txt
 done
-for files in `find $1/data/*data17*/*`
+for files in `find $1/*data17*.root`
 do
-	echo $files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/data17.txt
+	echo $files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/data17.txt
 done
 
-for files in `find $1/data/*data18*/*`
+for files in `find $1/*data18*.root`
 do
-	echo $files >> $ttH_fakes_DIR/datafiles/xTFW/v2/run/data18.txt
+	echo $files >> $ttH_fakes_DIR/datafiles/tthML/v2/run/data18.txt
 done
 fi
