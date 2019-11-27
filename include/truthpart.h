f9b118a0a592e5b4e86322d7ac1b404c2109a2df
Boyang Li <boyang.li@cern.ch>
2019-11-27 14:50:35 +0800

    forgot header

---
+++ include/truthpart.h
@@ -0,0 +1,17 @@
+#include "TLorentzVector.h"
+
+class truthpart
+{
+public:
+	truthpart(int _pdg, TLorentzVector _p4): pdg(_pdg), p4(_p4) {}
+	~truthpart(){}
+	truthpart *mother = NULL;
+	truthpart *twin = NULL;
+	std::vector<truthpart*> children;
+	TLorentzVector p4;
+
+	int pdg;
+	int barcode;
+	int motherbarcode = -1;
+	std::vector<int> childrenbarcode;
+};
