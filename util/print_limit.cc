645d84dcdea550052db0f8a6da8e4809cebcf75c
Boyang Li <boyang.li@cern.ch>
2020-03-26 13:07:30 +0800

    add print

---
+++ util/print_limit.cc
@@ -0,0 +1,35 @@
+#include "makechart.h"
+#include "TFile.h"
+#include "TH1D.h"
+using namespace std;
+
+void setlimit(LatexChart *chart, TString filename, string row, string column){
+	TFile thefile(filename);
+	if(thefile.IsZombie()) return;
+	TH1D *thehist = (TH1D*) thefile.Get("limit");
+	printf("chart->set(%f,%f,%f)\n", thehist->GetBinContent(2),thehist->GetBinContent(4)-thehist->GetBinContent(2),thehist->GetBinContent(2)-thehist->GetBinContent(5));
+	chart->set(row,column,thehist->GetBinContent(2),thehist->GetBinContent(4)-thehist->GetBinContent(2),thehist->GetBinContent(2)-thehist->GetBinContent(5));
+}
+
+int main(int argc, char const *argv[])
+{
+	TString variable = "BDTG_test";
+	vector<TString> signals = {"uH","cH"};
+	vector<TString> signalstitle = {"tuH","tcH"};
+	vector<TString> channels = {"2j","3j","2tau"};
+	vector<TString> channelstitle = {"STH $\\tlhad$", "TTH $\\tlhad$", "$l\\thadhad$"};
+	LatexChart *chart = new LatexChart("limit");
+	for (int isig = 0; isig < signals.size(); ++isig)
+	{
+		for (int ichan = 0; ichan < channels.size(); ++ichan)
+		{
+			TString jobname = variable + "_" + signals[isig] + "_" + channels[ichan];
+			setlimit(chart, jobname + "/Limits/" + jobname + ".root", signalstitle[isig].Data(), channelstitle[ichan].Data());
+		}
+		TString jobname = variable + "_" + signals[isig];
+		setlimit(chart, jobname + "/Limits/" + jobname + ".root", signalstitle[isig].Data(), "Combined");
+	}
+
+	chart->print("limits");
+	return 0;
+}
