c5b48448813bf63a7aa77bf380d2a4a2ba3892f1
Boyang Li <boyang@localhost.localdomain>
2020-08-01 03:20:58 -0700

    support multi c++ version

--- setup.sh
+++ setup.sh
@@ -14,4 +14,14 @@ if [ -z ${TOOL_DIR} ] ; then
 	return
 fi
 
-mkdir build ; cd build ; cmake .. ; make install ; cd ..
+for word in `root-config --cflags`
+do
+	if [[ "$word" =~ "-std=" ]] ; then
+		CXX_STANDARD=`echo $word | tr -dc '0-9'`
+		break;
+	fi
+done
+
+echo "-- Detected root CXX standard:" $CXX_STANDARD
+
+mkdir build ; cd build ; cmake .. -DCMAKE_CXX_STANDARD=$CXX_STANDARD ; make install ; cd ..
