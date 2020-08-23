d283e628608c2ed21ac7a2a4d1be68e25af58c2c
Boyang Li <boyang@localhost.localdomain>
2020-08-23 15:56:42 +0800

    multicore compilation

--- setup.sh
+++ setup.sh
@@ -14,6 +14,13 @@ if [ -z ${TOOL_DIR} ] ; then
 	return
 fi
 
+
+LAUNCHER="-DCMAKE_CXX_COMPILER_LAUNCHER=distcc"
+command -v distcc >/dev/null 2>&1 || { 
+	echo >&2 "distcc is not installed, distributed compilation is disabled.";
+	LAUNCHER=
+}
+
 for word in `root-config --cflags`
 do
 	if [[ "$word" =~ "-std=" ]] ; then
@@ -36,4 +43,4 @@ done
 
 echo "-- Detected root CXX standard:" $CXX_STANDARD
 
-mkdir build ; cd build ; cmake .. -DCMAKE_CXX_STANDARD=$CXX_STANDARD ; make install ; cd ..
+mkdir -p build ; cd build ; cmake .. -DCMAKE_CXX_STANDARD=$CXX_STANDARD $LAUNCHER; make -j4 install ; cd ..
