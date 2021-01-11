f0b3162ebd97bde4a534757e194dd71f7cd9d3cb
boyang <boyang.li@cern.ch>
2021-01-11 14:50:03 +0800

    closureTest

--- src/nominal.C
+++ src/nominal.C
@@ -1105,7 +1105,11 @@ void nominal::fillhist(histSaver* plots, TString region, TString sample, TString
   if(plotProng) prongname = prongname + "_" + char('0'+ taus_n_charged_tracks->at(0))+"prong";
   if(debug) printf("nominal::fillhist\n");
   if(dobwp[bwps[1]] == 1 && taus_b_tagged->at(0)) plots->fill_hist(sample,region+prongname + "_" + bwps[1],NP);
-  if(dovetobwp[bwps[1]] == 1 && !taus_b_tagged->at(0)) plots->fill_hist(sample,region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
+  if(dovetobwp[bwps[1]] == 1 && !taus_b_tagged->at(0)) {
+    plots->fill_hist(sample,region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
+    if(BDTG_test<-0.6) plots->fill_hist(sample,region+"_lowBDT"+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
+  }
+
 }
 
 void nominal::readweightsysmap(int dsid, TString framework){
