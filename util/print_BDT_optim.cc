7e149020dbf1c0d4097f7ad99df737b037f21c9e
boyang <boyang.li@cern.ch>
2021-01-17 17:26:38 +0800

    print BDT optim result

---
+++ util/print_BDT_optim.cc
@@ -0,0 +1,35 @@
+#include "LatexChart.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "common.h"
+#include "fcnc_include.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	std::map<string, string> regionTitles;
+
+	string framework = "tthML";
+
+	regionTitles["reg1l2tau1bnj_os"] = "l\\thadhad";
+	regionTitles["reg1l1tau1b1j_ss"] = "l\\tauhad 1j";
+	regionTitles["reg1l1tau1b2j_os"] = "STH \\tlhad";
+	regionTitles["reg1l1tau1b2j_ss"] = "l\\tauhad 2j";
+	regionTitles["reg1l1tau1b3j_os"] = "TTH \\tlhad";
+
+	LatexChart chart("BDTOptimResult");
+	ifstream inputfile(argv[1]);
+	char inputline[200];
+	while(!inputfile.eof()){
+		inputfile.getline(inputline,200);
+		if(strlen(inputline)==0) continue;
+		if(inputline[0]=='#') continue;
+		int ncut,ntree;
+		char channel[200];
+		sscanf(inputline,"%s %d %d", &channel, ncut, ntree);
+		chart.set(regionTitles[channel],"NCuts", (float)ncut);
+		chart.set(regionTitles[channel],"NTrees", (float)ntree);
+	}
+	chart.print(string(TABLE_DIR) + "/BDT/" + framework + "_OptimResult");
+	return 0;
+}
+
