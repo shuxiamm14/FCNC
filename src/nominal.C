a8f3274713e56ac531f9b039364f45b7149a80ca
boyang <boyang.li@cern.ch>
2021-01-19 16:29:28 +0800

    fix bug

--- src/nominal.C
+++ src/nominal.C
@@ -1210,8 +1210,10 @@ void nominal::fillhist(histSaver* plots, TString region, TString sample, TString
         } 
       }
       plots->fill_hist(sample,region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : (passReduce3Cut?"_highmet":"_highmet_SB")),NP);
+    }else{
+      if(BDTG_test<-0.6 && !plotProng) plots->fill_hist(sample,"lowBDT_" + region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
+      plots->fill_hist(sample,region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
     }
-    if(BDTG_test<-0.6) plots->fill_hist(sample,"lowBDT_" + region+prongname + "_veto" + bwps[1] + (etmiss < 20*GeV? "_lowmet" : "_highmet"),NP);
   }
 
 }
